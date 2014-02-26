87996285e8f5adaa9dc8353ef629c81337ea264d
codiller <cody@topleftcreative.com>
2014-02-26 11:47:44 -0800

    Updated short code

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -1,8 +1,8 @@
 <?php
 
 function tlc_online_application_url() {
-	$application_url = '<a class="btn-lg" href="' . genesis_get_option( 'application-url', 'tlc-options' ) . '" target="_blank">APPLY NOW!</a>';
-	return $application_url;
+	$tlc_application_url = '<a class="btn-lg" href="' . genesis_get_option( 'application_url', 'tlc_options' ) . '" target="_blank">APPLY NOW!</a>';
+	return $tlc_application_url;
 }
 add_shortcode( 'application_url', 'tlc_online_application_url' );
 
