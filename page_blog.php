395f7c6ee3110ef3276c6618687ba166c4d77b79
Cody D Martens <codiller@MacBook-Pro.local>
2014-11-03 15:45:35 -0800

    Renamed Page Files

---
+++ page_blog.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ *
+ * Template Name: Blog
+ *
+ */
+
+/*
+ *
+ * Replace Primary Sidebar with Blog Sidebar on Blog Page
+ *
+ */
+add_action( 'genesis_sidebar', 'tlc_blog_sidebar', 4 );
+function tlc_blog_sidebar() {
+	
+	global $post;
+	
+	remove_action( 'genesis_sidebar', 'genesis_do_sidebar' ); ?>
+	
+	<aside class="sidebar-entry">
+		<?php dynamic_sidebar( 'blog-sidebar' ); ?>
+	</aside><!-- end .sidebar-entry-->
+	
+<?php }
+
+genesis();
+
+?>
